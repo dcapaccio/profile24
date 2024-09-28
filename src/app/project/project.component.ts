f9b00a0644550b692fbae2f3fae1132129c851ef
Danny <d.capaccio@proton.me>
2024-09-28 14:56:59 -0400

    some updates.

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project',
   standalone: true,
   imports: [],
   templateUrl: './project.component.html',
-  styleUrl: './project.component.scss'
+  styleUrl: './project.component.scss',
 })
 export class ProjectComponent {
-
+  @Input({ required: true }) mainimage: string = 'assets/images/test.jpg';
+  @Input() description: string = 'Test description of this project.';
+  @Input() name: string = 'Project Name';
+  @Input() year: string = '2021';
+  @Input() client: string = 'client name, if there is one.';
+  @Input() exhibit: string = "If there's an exhibit.";
 }
